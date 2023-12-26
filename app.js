d0f043d127236403d12995bf122831da20008b04
Celia <celiamillanalemany@gmail.com>
2023-12-26 11:04:26 +0100

    creating environment variables

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express  from "express";
 import cors from 'cors'
+import 'dotenv/config'
+
+
 
 const app = express();
 app.use(cors())
 
-const PORT = 3000
+
+
+const PORT = process.env.PORT || 8000
  
 app.listen(PORT, ()=> {
     console.log(`tu app esta lista por http://localhost:${PORT}`)
 })
+
